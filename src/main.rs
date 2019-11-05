87cc4217a02e5f6877c63205100e78b2864785f8
Jonas Platte <jplatte+git@posteo.de>
2019-11-05 22:39:13 +0100

    Use anyhow, ? operator

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-extern crate i3ipc;
-
 use std::{env, process};
+
+use anyhow::Context as _;
 use i3ipc::I3Connection;
 
 enum Command {
@@ -13,7 +13,7 @@ fn usage() -> ! {
     process::exit(1)
 }
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     let mut args = env::args();
     let command = match args.nth(1) {
         Some(ref arg) if arg == "prev" => Command::Prev,
@@ -21,23 +21,25 @@ fn main() {
         _ => usage(),
     };
 
-    let mut i3conn = I3Connection::connect().unwrap();
+    let mut i3conn = I3Connection::connect()?;
 
-    let workspaces = i3conn.get_workspaces().unwrap().workspaces;
-    let focused_ws = workspaces.iter().find(|ws| ws.focused).expect("No focused workspace!");
+    let workspaces = i3conn.get_workspaces()?.workspaces;
+    let focused_ws = workspaces.iter().find(|ws| ws.focused).context("No focused workspace!")?;
 
     let mut same_output_workspaces = workspaces.iter().filter(|ws| ws.output == focused_ws.output);
 
     match command {
         Command::Prev => {
-            if same_output_workspaces.next().unwrap().name != focused_ws.name {
-                i3conn.run_command("workspace prev_on_output").unwrap();
+            if same_output_workspaces.next().context("no workspaces")?.name != focused_ws.name {
+                i3conn.run_command("workspace prev_on_output")?;
             }
         }
         Command::Next => {
-            if same_output_workspaces.last().unwrap().name != focused_ws.name {
-                i3conn.run_command("workspace next_on_output").unwrap();
+            if same_output_workspaces.last().context("no workspaces")?.name != focused_ws.name {
+                i3conn.run_command("workspace next_on_output")?;
             }
         }
     };
+
+    Ok(())
 }
